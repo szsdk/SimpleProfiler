9857f2736c513d881fd5174c9d67daf4346eb760
szsdk <shen_zhou@u.nus.edu>
2022-08-20 08:26:24 +0200

    remove progress bar

--- sp.py
+++ sp.py
@@ -1,11 +1,14 @@
-import logging
-from rich.logging import RichHandler
-from rich.progress import Progress
 from pathlib import Path
 import tempfile
+import logging
+
+from rich.logging import RichHandler
+from rich.console import Console
+from rich.syntax import Syntax
 import platform
 import numpy as np
 import toml
+
 import utils
 
 
@@ -14,11 +17,13 @@ logging.basicConfig(
     level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
 )
 
+
 def cpu_info():
     try:
         import cpuinfo
     except ImportError:
         logging.warning("Cannot import cpuinfo.")
+        return
     return cpuinfo.get_cpu_info()
 
 
@@ -38,8 +43,8 @@ def np_copy(nbytes):
     a = utils.rand_array(nbytes)
     b = a.copy()
     info = utils.timeit(lambda: np.copyto(b, a), n=7)
-    info["memory size"] = f"{a.nbytes / (1 << 20)} MB"
-    info["copy speed (GB/s)"] = a.nbytes / (1 << 30) / info['mean(s)']
+    info["memory size (MB)"] = a.nbytes / (1 << 20)
+    info["copy speed (GB/s)"] = a.nbytes / (1 << 30) / info["mean(s)"]
     return info
 
 
@@ -47,7 +52,7 @@ def np_float_sum(nbytes):
     a = utils.rand_array(nbytes)
     info = utils.timeit(lambda: np.sum(a), n=7)
     info["array size"] = len(a)
-    info["sum speed (G element/s)"] = len(a) * 1e-9 / info['mean(s)']
+    info["sum speed (G element/s)"] = len(a) * 1e-9 / info["mean(s)"]
     return info
 
 
@@ -62,12 +67,13 @@ def h5_IO(nbytes):
     with tempfile.NamedTemporaryFile(dir=Path(), suffix=".h5") as f:
         with h5py.File(f.name, "w") as fp:
             info = utils.timeit(
-                lambda: fp.create_dataset(utils.rand_str(), data=a), n=10)
-        result["writing speed (GB/s)"] = nbytes * 1e-9 / info['mean(s)']
+                lambda: fp.create_dataset(utils.rand_str(), data=a), n=10
+            )
+        result["writing speed (GB/s)"] = nbytes * 1e-9 / info["mean(s)"]
         with h5py.File(f.name, "r") as fp:
             keys = iter(fp.keys())
             info = utils.timeit(lambda: fp[next(keys)][...], n=10)
-        result["reading speed (GB/s)"] = nbytes * 1e-9 / info['mean(s)']
+        result["reading speed (GB/s)"] = nbytes * 1e-9 / info["mean(s)"]
     return result
 
 
@@ -79,6 +85,7 @@ def prime_benchmark(nb_primes, method: str):
             from sci_benchmark.cython_func import primes
         elif method == "numba":
             from sci_benchmark.numba_func import primes
+
             primes(20)  # warmup
     except ImportError:
         logging.warning(f"Cannot import sci_benchmark {method}")
@@ -91,28 +98,29 @@ def prime_benchmark(nb_primes, method: str):
 
 
 result = dict()
-with Progress() as progress:
-    task1 = progress.add_task("", total=None)
-    progress.update(task1, advance=1, description="system info")
-    result["system"] = system_info()
-    progress.update(task1, advance=1, description="cpu info")
-    result["cpu"] = cpu_info()
-    progress.update(task1, advance=1, description="numpy copy (small)")
-    result["numpy copy (small)"] = np_copy(1 << 18)
-    progress.update(task1, advance=1, description="numpy copy (large)")
-    result["numpy copy (large)"] = np_copy(1 << 27)
-    progress.update(task1, advance=1, description="numpy float sum (small)")
-    result["numpy float sum (small)"] = np_float_sum(1 << 18)
-    progress.update(task1, advance=1, description="numpy float sum (large)")
-    result["numpy float sum (large)"] = np_float_sum(1 << 27)
-    progress.update(task1, advance=1, description="HDF5 IO (small)")
-    result["HDF5 IO (small)"] = h5_IO(1 << 18)
-    progress.update(task1, advance=1, description="HDF5 IO (large)")
-    result["HDF5 IO (large)"] = h5_IO(1 << 27)
-    progress.update(task1, advance=1, description="pybind11 primes")
-    result["pybind11 primes"] = prime_benchmark(300000, "pybind11")
-    progress.update(task1, advance=1, description="cython primes")
-    result["cython primes"] = prime_benchmark(300000, "cython")
-    progress.update(task1, advance=1, description="numba primes")
-    result["numba primes"] = prime_benchmark(300000, "numba")
-print(toml.dumps(result))
+result["system"] = system_info()
+logging.info("system information")
+result["cpu"] = cpu_info()
+logging.info("cpu information")
+logging.info("numpy array (small) in memory copy")
+result["numpy copy (small)"] = np_copy(1 << 18)
+logging.info("numpy array (large) in memory copy")
+result["numpy copy (large)"] = np_copy(1 << 27)
+logging.info("sum of a samll numpy float array")
+result["numpy float sum (small)"] = np_float_sum(1 << 18)
+logging.info("sum of a large numpy float array")
+result["numpy float sum (large)"] = np_float_sum(1 << 27)
+logging.info("HDF5 IO (small)")
+result["HDF5 IO (small)"] = h5_IO(1 << 18)
+logging.info("HDF5 IO (large)")
+result["HDF5 IO (large)"] = h5_IO(1 << 27)
+logging.info("primes (pybind11)")
+result["pybind11 primes"] = prime_benchmark(300000, "pybind11")
+logging.info("primes (cython)")
+result["cython primes"] = prime_benchmark(300000, "cython")
+logging.info("primes (numba)")
+result["numba primes"] = prime_benchmark(300000, "numba")
+
+console = Console()
+syntax = Syntax(toml.dumps(result), "toml", background_color="default")
+console.print(syntax)
